24403f91e287d099131fc0b2c2f3cb2e72c4b983
Bogdan Vatamanu <vatamanu.bogdan41@gmail.com>
2025-01-13 17:03:17 +0200

    feat(web-client): Add login form

--- web-client/src/pages/LoginForm.tsx
+++ web-client/src/pages/LoginForm.tsx
@@ -1,4 +1,13 @@
+import InputField from "../components/InputField";
 
 export default function LoginForm() {
-    return <h1 className="text-zinc-100 text-xl"> Login Form Content </h1>
+    return (         
+        <>
+            <InputField placeholder='Email'/>
+            <InputField placeholder='Password' hidden={true}/>
+        
+            <button className='self-start -mt-8 px-1 text-sm text-teal-500'> Did you forget your password? </button>
+            <button className='w-28 h-14 rounded-lg border-2 border-teal-500 text-zinc-100 font-medium hover:bg-teal-500 text-lg bg-transparent'> Login </button>
+        </>
+    );
 } 
