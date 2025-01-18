57389b661db0011e1bb24a06f3de58bb446c4e94
Bogdan Vatamanu <vatamanu.bogdan41@gmail.com>
2025-01-18 23:41:55 +0200

    feat(web-client): Set "AuthProvider" on the application root and set "HomePage" as protected route

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -5,24 +5,25 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import AuthenticationLayout from './pages/AuthenticationLayout';
 
 import './index.css'
-import LoginForm from './pages/LoginForm';
+import SignInForm from './pages/SignInForm';
 import RegisterForm from './pages/RegisterForm';
 import { AuthProvider } from './components/AuthProvider';
 import HomePage from './pages/HomePage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path='auth' element={<AuthenticationLayout />} >
-            <Route index element={<LoginForm/>}/>
-            <Route path='login' element={<LoginForm/>}/>
-            <Route path='register' element={<RegisterForm/>}/>
-          </Route>
-          <Route path='home' element={<HomePage/>} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path='auth' element={<AuthenticationLayout />} >
+              <Route index element={<SignInForm/>}/>
+              <Route path='login' element={<SignInForm/>}/>
+              <Route path='register' element={<RegisterForm/>}/>
+            </Route>
+            <Route path='home' element={<ProtectedRoute> <HomePage/> </ProtectedRoute>} />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
-    </AuthProvider>
   </React.StrictMode>
 )
