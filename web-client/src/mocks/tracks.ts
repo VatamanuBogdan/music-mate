1fdbefb69280b93ceb9da91c3fbc76e0fe8805ed
Bogdan Vatamanu <vatamanu.bogdan41@gmail.com>
2025-02-04 18:20:58 +0200

    feat(web-client): Add floating player component and update linting rules (#4)
    * feat(web-client): Update eslint rules and add prettier
    
    * feat(web-client): Add new floating player component and remove the old one

---
+++ web-client/src/mocks/tracks.ts
@@ -0,0 +1,18 @@
+import { Track } from "../types/Track";
+
+export const trackMocks: Track[] = [
+    {
+        id: '1',
+        name: 'St. Anger',
+        artist: 'Metallica',
+        imageUrl: 'https://cdn.media.amplience.net/i/metallica/st-anger_cover?fmt=auto&maxW=1050',
+        duration: 348
+    },
+    {
+        id: '2',
+        name: 'Dangerous',
+        artist: 'David Guetta',
+        imageUrl: 'https://i.scdn.co/image/ab67616d0000b27322f21ef4e9da48c31170a418',
+        duration: 204
+    }
+]
