b2bc18b514b12dfcf35a713e84705e0b0d19f6e8
Bogdan Vatamanu <vatamanu.bogdan41@gmail.com>
2025-02-04 18:11:49 +0200

    feat(web-client): Update eslint rules and add prettier

--- web-client/eslint.config.js
+++ web-client/eslint.config.js
@@ -1,28 +1,100 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import eslintPrettierPlugin from 'eslint-plugin-prettier';
+import importPlugin from 'eslint-plugin-import';
+import eslintReactPlugin from 'eslint-plugin-react';
+import eslintReactHooksPlugin from 'eslint-plugin-react-hooks';
+import eslintReactRefreshPlugin from 'eslint-plugin-react-refresh';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptEslintParser from '@typescript-eslint/parser';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+export default [{
+    files: ['src/**/*.{js,ts,jsx,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+        parser: typescriptEslintParser,
+
+        parserOptions: {
+            ecmaVersion: 2020,
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+            },
+            ecmaFeatures: {
+                jsx: true,
+            },
+        }
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+        'prettier': eslintPrettierPlugin,
+        '@typescript-eslint': typescriptEslintPlugin,
+        'react': eslintReactPlugin,
+        'react-hooks': eslintReactHooksPlugin,
+        'react-refresh': eslintReactRefreshPlugin,
+        'import': importPlugin
     },
+    ignores: [
+        '**/node_modules/**', 
+        '**/eslint.config.mjs', 
+        '**/dist/**', 
+        '**/build/**'
+    ],
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+        'prettier/prettier': 'error',
+
+        /* General */
+        'no-unused-vars': ['off'],
+        'no-plusplus': 'error',
+        'no-undef': 'off',
+        'no-console': 'warn',
+        'eol-last': 'error',
+        'prefer-const': 'error',
+        'no-var': 'error',
+        'no-eval': 'error',
+        'no-fallthrough': 'error',
+        'grouped-accessor-pairs': ['error', 'getBeforeSet'],
+        'eqeqeq': ['error', 'always'],
+        'arrow-parens': ['error', 'always'],
+        'object-curly-spacing': ['error', 'always'],
+        'curly': ['error', 'all'],
+        'prefer-template': 'error',
+        'no-useless-concat': 'error',
+
+        /* Typescript */
+        '@typescript-eslint/no-unused-vars': ['error'],
+        '@typescript-eslint/explicit-module-boundary-types': 'error',
+        '@typescript-eslint/no-explicit-any': 'error',
+
+        /* React */
+        'react/jsx-uses-react': 'error',
+        'react/jsx-uses-vars': 'error',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+
+        /* Imports */
+        'import/no-duplicates': 'error',
+        'import/no-unused-modules': 'error',
+        'import/no-relative-parent-imports': 'error',
+        'import/newline-after-import': 'error',
+        'sort-imports': [
+            'error',
+            {
+                'ignoreCase': false,
+                'ignoreDeclarationSort': true,
+                'ignoreMemberSort': false,
+                'memberSyntaxSortOrder': ['none', 'all', 'multiple', 'single']
+            }
+        ],
+        'import/order': [
+            'error',
+            {
+                'groups': [
+                    'builtin',
+                    'external',
+                    'internal',
+                    ['parent', 'sibling', 'index']
+                ],
+                'newlines-between': 'always',
+                'alphabetize': { 'order': 'asc', 'caseInsensitive': true }
+            }
+        ],
+    }
+}];
