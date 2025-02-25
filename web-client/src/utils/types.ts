c9fb5a86d33c1cccff9aef9812829b1de78a41da
Bogdan Vatamanu <vatamanu.bogdan41@gmail.com>
2025-02-25 13:33:36 +0200

    feat(web-client): Add data attribute helper

--- web-client/src/utils/types.ts
+++ web-client/src/utils/types.ts
@@ -19,3 +19,42 @@ export interface Duration {
 
 export type DurationSeconds = number;
 export const isDurationSeconds = isNumber;
+
+export interface DataAttribute<T extends string> {
+    readonly name: string;
+    value(element: HTMLElement): T | null;
+    closestValue(element: HTMLElement): T | null;
+    set(value: T): Record<string, T>;
+}
+
+class DataAttributeImpl<T extends string> implements DataAttribute<T> {
+    public readonly name: string;
+    private selector: string;
+
+    constructor(name: string) {
+        if (name.startsWith('data-')) {
+            this.name = name;
+        } else {
+            this.name = `data-${name}`;
+        }
+
+        this.selector = `[${this.name}]`;
+    }
+
+    public value(element: HTMLElement): T | null {
+        return element.getAttribute(this.name) as T;
+    }
+
+    public closestValue(element: HTMLElement): T | null {
+        const newElement = element.closest(this.selector);
+        return (newElement?.getAttribute(this.name) ?? null) as T;
+    }
+
+    public set(value: T): Record<string, T> {
+        return { [this.name]: value };
+    }
+}
+
+export function createDataAttribute<T extends string = string>(name: string): DataAttribute<T> {
+    return new DataAttributeImpl<T>(name);
+}
